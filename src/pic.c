8cf0a288c60566973f436f74c46f7b88c815cd38
Sean Freiburg <frycicle@gmail.com>
2014-04-24 13:27:46 -0500

    full screen

--- src/pic.c
+++ src/pic.c
@@ -2,9 +2,16 @@
 
 static Window *window;
 static TextLayer *text_layer;
+static BitmapLayer *icon_layer;
+static GBitmap *icon_bitmap = NULL;
+//144x168
+
+static uint32_t WEATHER_ICONS[] = {
+  RESOURCE_ID_IMAGE_SUN
+};
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+  text_layer_set_text(text_layer, "Penis");
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -25,10 +32,17 @@ static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+
+
   text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
   text_layer_set_text(text_layer, "Press a button");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
+
+  icon_layer = bitmap_layer_create(bounds);
+  layer_add_child(window_layer, bitmap_layer_get_layer(icon_layer));
+  icon_bitmap = gbitmap_create_with_resource(WEATHER_ICONS[0]);
+  bitmap_layer_set_bitmap(icon_layer, icon_bitmap);
 }
 
 static void window_unload(Window *window) {
@@ -37,12 +51,14 @@ static void window_unload(Window *window) {
 
 static void init(void) {
   window = window_create();
+
   window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
   const bool animated = true;
+  window_set_fullscreen(window, true);
   window_stack_push(window, animated);
 }
 
